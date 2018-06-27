cfc0a856dc3af8da77a56e641370f3ff2317d604
wb-lhb405348 <wb-lhb405348@antfin.com>
2018-06-27 18:13:22 +0800

    1：添加log4j2部分功能 2：添加pom文件注释

--- src/test/java/com/gzwork/test/SpringTest.java
+++ src/test/java/com/gzwork/test/SpringTest.java
@@ -2,6 +2,8 @@ package com.gzwork.test;
 
 import com.gzwork.entity.User;
 import com.gzwork.servers.UserServ;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +15,13 @@ public class SpringTest {
     @Autowired
     private UserServ userServ;
 
+    private Logger logger = LogManager.getLogger(SpringTest.class);
+
     @Test
     public void getUserById(){
         User user=new User();
         user.setUserId(1);
+        logger.info("log4j2");
         System.out.println(this.userServ.getUserById(user));
     }
 }
